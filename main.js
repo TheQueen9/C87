18d07b85aa5e969f484dbbf0c130ee8da23143fb
TheQueen9 <115858690+TheQueen9@users.noreply.github.com>
2023-02-17 15:21:43 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var block_image_object= "";
 function new_image(get_image)
 {
 	// to upload images
-	fabric.Image.fromURL(get_imagae, function(Img){
+	fabric.Image.fromURL(get_image, function(Img){
 		block_image_object = Img;
 
 		block_image_object.scaleToWidth(block_image_width)
